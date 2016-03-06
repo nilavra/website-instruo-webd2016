d25f066c83f1af73a8d8b25b24587322f45c0b8e
nilavra92 <nilavra1992@gmail.com>
2016-03-06 23:49:42 +0530

    kichuta done

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,72 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function(){
-	//$('footer').css({width:'1905px'});
+$(document).ready(function()
+{
+	var current_path = window.location.pathname.split('/').pop();
+	//if not logged in, redirect to index.html
+	if(!$.session.get('userid') && current_path !== 'index.html' )
+		window.location.replace('index.html');
+	
+	$('#form-signin').submit(function()
+	{
+		$.session.clear();
+		
+		var username = $('input[name=username]').val();
+		var password = $('input[name=password]').val();
+		var password_md5 = $.md5(password);
+		
+		var found = false;
+		
+		$.getJSON('users.json', function(result) 
+		{
+			for (var key in result)
+			{
+				if (result.hasOwnProperty(key))
+				{
+					// here you have access to
+					var USERID = result[key].userid;
+					var USERNAME = result[key].username;
+					var PASSWORD = result[key].password;
+
+					if(USERNAME === username && PASSWORD === password_md5)
+					{
+						found = true;
+						$.session.set('userid', USERID);
+						$.session.set('username', USERNAME);
+						break;
+					}
+				}
+			}
+		});
+		
+		if(found)
+			window.location.replace('menu.html');
+		else
+		{
+			$.notify({
+				title: '<strong>Invalid Login!</strong>',
+				message: 'The username-password provided by you is incorrect.'
+			},{
+				type: 'danger',
+				placement: {
+					from: 'top',
+					align: 'center'
+				},
+				delay:300000
+			});
+			
+			$('input[name=username]').val('');
+			$('input[name=password]').val('');
+			
+			$('input[name=username]').focus();
+		}
+		
+		
+		return false;
+	});
+	
+	
+	
 });
 
---
+++ readme.md
@@ -0,0 +1,25 @@
+1. js cookie - sessions:
+http://stackoverflow.com/questions/2257631/how-create-a-session-using-javascript
+
+2. load local json
+http://stackoverflow.com/questions/27360326/how-do-i-load-my-local-json-file-just-using-javascript
+http://stackoverflow.com/questions/9066335/ajax-get-not-working-with-local-json-file
+
+
+North Indian
+Fast Food
+South Indian
+Packaged Food
+Beverages
+Chinese
+
+
+
+
+http://html.themographics.com/dhaba/restaurant/menu.html
+
+https://www.smashingmagazine.com/2014/02/create-client-side-shopping-cart/
+
+http://isotope.metafizzy.co/filtering.html
+
+pics: 250 x 250
